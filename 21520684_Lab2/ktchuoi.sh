905248f6e537296658d85fe4307ebb004df61b9e
trandang2003 <21520684@gm.uit.edu.vn>
2022-11-20 17:12:39 +0700

    first commit

--- 21520684_Lab2/ktchuoi.sh
+++ 21520684_Lab2/ktchuoi.sh
@@ -5,12 +5,15 @@ folder=$2
 file_result=$3
 count=0
 
+
 if [ -d $folder ];then
+	cd $folder
 	for file in $(ls *);do
 		if grep -q $string $file;then
 			count=$(($count+1))
 		fi
 	done
+	cd ..
 	if [ $count = 0 ];then
 		echo "Trong thu muc $folder khong co tap tin nao chua chuoi $string">$file_result
 	else 
